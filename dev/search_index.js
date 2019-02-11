0fc76d30211a3bd82fc7e93ab5307fbf52b7c4a8
autodocs <autodocs>
2019-02-11 19:47:04 +0000

    build based on 1dfd42f

---
+++ dev/search_index.js
@@ -0,0 +1,11 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "Test of the documentation.See the docs for Expectations.jl for examples of ⁠LaTeX, etc."
+},
+
+]}
