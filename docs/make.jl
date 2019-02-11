1ffaec1b6aa4e82ef91ef9b341d6e0a1ee9ca327
Arnav Sood <soodarnav01@gmail.com>
2019-02-11 11:41:52 -0800

    forget about mkdocs for now

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,3 @@ using Documenter, SimpleDifferentialOperators
 
 # Compile the raw documentation.
 makedocs(sitename = "SimpleDifferentialOperators.jl")
-
-# MkDocs
-deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/QuantEcon/SimpleDifferentialOperators.jl.git",
-    target = "site",
-    make = () -> run(`mkdocs build`)
-)
