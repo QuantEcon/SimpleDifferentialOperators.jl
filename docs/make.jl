1dfd42fa52ea47b32375f6aaba834e5bf10d25d3
Arnav Sood <soodarnav01@gmail.com>
2019-02-11 11:43:19 -0800

    last tweak

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,8 @@ using Documenter, SimpleDifferentialOperators
 
 # Compile the raw documentation.
 makedocs(sitename = "SimpleDifferentialOperators.jl")
+
+# Deploy to git
+deploydocs(
+    repo = "github.com/QuantEcon/SimpleDifferentialOperators.jl.git",
+)
