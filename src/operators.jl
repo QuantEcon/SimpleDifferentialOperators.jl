8c7702f1bdf606eb9cd32d3f3d271983a3c00776
Arnav Sood <soodarnav01@gmail.com>
2019-02-14 13:20:57 -0800

    make dispatch tests value-independent
    [ci skip]

--- src/operators.jl
+++ src/operators.jl
@@ -3,20 +3,16 @@ diffusionoperators(x, BC1::BoundaryCondition, BC2::BoundaryCondition) = _diffusi
 
 # Uniform grid, (Reflecting, Reflecting)
 function _diffusionoperators(x::AbstractRange, BC1::Reflecting, BC2::Reflecting)
-    return (x = "Uniform", BC1 = "Reflecting", BC2 = "Reflecting")
 end
 
 # Uniform grid, (Mixed, Mixed)
 function _diffusionoperators(x::AbstractRange, BC1::Mixed, BC2::Mixed)
-    return (x = "Uniform", BC1 = "Mixed", BC2 = "Mixed")
 end
 
 # Irregular grid, (Reflecting, Reflecting)
 function _diffusionoperators(x::AbstractArray, BC1::Reflecting, BC2::Reflecting)
-    return (x = "Irregular", BC1 = "Reflecting", BC2 = "Reflecting")
 end
 
 # Irregular grid, (Mixed, Mixed)
 function _diffusionoperators(x::AbstractArray, BC1::Mixed, BC2::Mixed)
-    return (x = "Irregular", BC1 = "Mixed", BC2 = "Mixed")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,17 @@ using SimpleDifferentialOperators
 using Test, LinearAlgebra
 
 @testset "Dispatch Tests" begin
-    grids = [range(1.0, 10.0, length = 100), collect(range(1.0, 10.0, length = 100))]
-    BCs = [Reflecting(), Mixed(2.0), Absorbing(1.0, 2.0)] # list of all possible BCs
+    methodList = collect(methods(SimpleDifferentialOperators._diffusionoperators))
 
     # Test for defined cases
-    @test diffusionoperators(grids[1], BCs[1], BCs[1]) == (x = "Uniform", BC1 = "Reflecting", BC2 = "Reflecting")
-    @test diffusionoperators(grids[1], BCs[2], BCs[2]) == (x = "Uniform", BC1 = "Mixed", BC2 = "Mixed")
-    @test diffusionoperators(grids[2], BCs[1], BCs[1]) == (x = "Irregular", BC1 = "Reflecting", BC2 = "Reflecting")
-    @test diffusionoperators(grids[2], BCs[2], BCs[2]) == (x = "Irregular", BC1 = "Mixed", BC2 = "Mixed")
+    @test which(SimpleDifferentialOperators._diffusionoperators, (AbstractRange, Reflecting, Reflecting)) == methodList[1]
+    @test which(SimpleDifferentialOperators._diffusionoperators, (AbstractRange, Mixed, Mixed)) == methodList[2]
+    @test which(SimpleDifferentialOperators._diffusionoperators, (AbstractArray, Reflecting, Reflecting)) == methodList[3]
+    @test which(SimpleDifferentialOperators._diffusionoperators, (AbstractArray, Mixed, Mixed)) == methodList[4]
 
     # Test for error handling
+    grids = [range(1.0, 10.0, length = 100), collect(range(1.0, 10.0, length = 100))]
+    BCs = [Reflecting(), Mixed(2.0), Absorbing(1.0, 2.0)] # list of all possible BCs
     @test_throws MethodError diffusionoperators(grids[1], BCs[1], BCs[3])
     @test_throws MethodError diffusionoperators(grids[2], BCs[1], BCs[2])
 end
