3a0f565869d561a25d1957362d7ab200cdf1a36c
Arnav Sood <soodarnav01@gmail.com>
2019-02-14 11:29:42 -0800

    simplify tests and cut unimplemented cases

--- src/operators.jl
+++ src/operators.jl
@@ -1,23 +1,9 @@
 # General dispatcher
 diffusionoperators(x, BC1::BoundaryCondition, BC2::BoundaryCondition) = _diffusionoperators(x, BC1, BC2)
 
-#=
-    UNIFORM GRID
-=#
-
-# Uniform grid, two reflecting
+# Uniform grid, (Reflecting, Reflecting)
 function _diffusionoperators(x::AbstractRange, BC1::Reflecting, BC2::Reflecting)
     return (x = "Uniform", BC1 = "Reflecting", BC2 = "Reflecting")
-end
-
-# Uniform grid, (Reflecting, Mixed)
-function _diffusionoperators(x::AbstractRange, BC1::Reflecting, BC2::Mixed)
-    return (x = "Uniform", BC1 = "Reflecting", BC2 = "Mixed")
-end
-
-# Uniform grid, (Mixed, Reflecting)
-function _diffusionoperators(x::AbstractRange, BC1::Mixed, BC2::Reflecting)
-    return (x = "Uniform", BC1 = "Mixed", BC2 = "Reflecting")
 end
 
 # Uniform grid, (Mixed, Mixed)
@@ -25,76 +11,12 @@ function _diffusionoperators(x::AbstractRange, BC1::Mixed, BC2::Mixed)
     return (x = "Uniform", BC1 = "Mixed", BC2 = "Mixed")
 end
 
-# Uniform grid, (Reflecting, Absorbing)
-function _diffusionoperators(x::AbstractRange, BC1::Reflecting, BC2::Absorbing)
-    return (x = "Uniform", BC1 = "Reflecting", BC2 = "Absorbing")
-end
-
-# Uniform grid, (Absorbing, Reflecting)
-function _diffusionoperators(x::AbstractRange, BC1::Absorbing, BC2::Reflecting)
-    return (x = "Uniform", BC1 = "Absorbing", BC2 = "Reflecting")
-end
-
-# Uniform grid, (Absorbing, Absorbing)
-function _diffusionoperators(x::AbstractRange, BC1::Absorbing, BC2::Absorbing)
-    return (x = "Uniform", BC1 = "Absorbing", BC2 = "Absorbing")
-end
-
-# Uniform grid, (Absorbing, Mixed)
-function _diffusionoperators(x::AbstractRange, BC1::Absorbing, BC2::Mixed)
-    return (x = "Uniform", BC1 = "Absorbing", BC2 = "Mixed")
-end
-
-# Uniform grid, (Mixed, Absorbing)
-function _diffusionoperators(x::AbstractRange, BC1::Mixed, BC2::Absorbing)
-    return (x = "Uniform", BC1 = "Mixed", BC2 = "Absorbing")
-end
-
-#=
-    IRREGULAR GRID
-=#
-
-# Irregular grid, two reflecting
+# Irregular grid, (Reflecting, Reflecting)
 function _diffusionoperators(x::AbstractArray, BC1::Reflecting, BC2::Reflecting)
     return (x = "Irregular", BC1 = "Reflecting", BC2 = "Reflecting")
-end
-
-# Irregular grid, (Reflecting, Mixed)
-function _diffusionoperators(x::AbstractArray, BC1::Reflecting, BC2::Mixed)
-    return (x = "Irregular", BC1 = "Reflecting", BC2 = "Mixed")
-end
-
-# Irregular grid, (Mixed, Reflecting)
-function _diffusionoperators(x::AbstractArray, BC1::Mixed, BC2::Reflecting)
-    return (x = "Irregular", BC1 = "Mixed", BC2 = "Reflecting")
 end
 
 # Irregular grid, (Mixed, Mixed)
 function _diffusionoperators(x::AbstractArray, BC1::Mixed, BC2::Mixed)
     return (x = "Irregular", BC1 = "Mixed", BC2 = "Mixed")
-end
-
-# Irregular grid, (Reflecting, Absorbing)
-function _diffusionoperators(x::AbstractArray, BC1::Reflecting, BC2::Absorbing)
-    return (x = "Irregular", BC1 = "Reflecting", BC2 = "Absorbing")
-end
-
-# Irregular grid, (Absorbing, Reflecting)
-function _diffusionoperators(x::AbstractArray, BC1::Absorbing, BC2::Reflecting)
-    return (x = "Irregular", BC1 = "Absorbing", BC2 = "Reflecting")
-end
-
-# Irregular grid, (Absorbing, Absorbing)
-function _diffusionoperators(x::AbstractArray, BC1::Absorbing, BC2::Absorbing)
-    return (x = "Irregular", BC1 = "Absorbing", BC2 = "Absorbing")
-end
-
-# Irregular grid, (Absorbing, Mixed)
-function _diffusionoperators(x::AbstractArray, BC1::Absorbing, BC2::Mixed)
-    return (x = "Irregular", BC1 = "Absorbing", BC2 = "Mixed")
-end
-
-# Irregular grid, (Mixed, Absorbing)
-function _diffusionoperators(x::AbstractArray, BC1::Mixed, BC2::Absorbing)
-    return (x = "Irregular", BC1 = "Mixed", BC2 = "Absorbing")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,38 +4,14 @@ using Test, LinearAlgebra
 @testset "Dispatch Tests" begin
     grids = [range(1.0, 10.0, length = 100), collect(range(1.0, 10.0, length = 100))]
     BCs = [Reflecting(), Mixed(2.0), Absorbing(1.0, 2.0)] # list of all possible BCs
-    inputMatrix = collect(Base.product(grids, BCs, BCs))
-    for input in inputMatrix
-        res = diffusionoperators(input...)
-        # Test grid dispatch
-        if input[1] isa AbstractRange
-            @test res[1] == "Uniform"
-        elseif input[1] isa AbstractArray
-            @test res[1] == "Irregular"
-        else
-            @error "This should never happen."
-        end
 
-        # Test BC1
-        if input[2] isa Reflecting
-            @test res[2] == "Reflecting"
-        elseif input[2] isa Mixed
-            @test res[2] == "Mixed"
-        elseif input[2] isa Absorbing
-            @test res[2] == "Absorbing"
-        else
-            @error "This should never happen."
-        end
+    # Test for defined cases
+    @test diffusionoperators(grids[1], BCs[1], BCs[1]) == (x = "Uniform", BC1 = "Reflecting", BC2 = "Reflecting")
+    @test diffusionoperators(grids[1], BCs[2], BCs[2]) == (x = "Uniform", BC1 = "Mixed", BC2 = "Mixed")
+    @test diffusionoperators(grids[2], BCs[1], BCs[1]) == (x = "Irregular", BC1 = "Reflecting", BC2 = "Reflecting")
+    @test diffusionoperators(grids[2], BCs[2], BCs[2]) == (x = "Irregular", BC1 = "Mixed", BC2 = "Mixed")
 
-        # Test BC2
-        if input[3] isa Reflecting
-            @test res[3] == "Reflecting"
-        elseif input[3] isa Mixed
-            @test res[3] == "Mixed"
-        elseif input[3] isa Absorbing
-            @test res[3] == "Absorbing"
-        else
-            @error "This should never happen."
-        end
-    end
+    # Test for error handling
+    @test_throws MethodError diffusionoperators(grids[1], BCs[1], BCs[3])
+    @test_throws MethodError diffusionoperators(grids[2], BCs[1], BCs[2])
 end
