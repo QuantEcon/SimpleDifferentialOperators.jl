9a4c0fcbfcf41d29fe610b36f0692633fc247d3d
Arnav Sood <soodarnav01@gmail.com>
2019-02-15 10:22:24 -0800

    add benchmarks code and json of reference results

---
+++ test/runbenchmarks.jl
@@ -0,0 +1,25 @@
+# Pkg deps
+using BenchmarkTools, SimpleDifferentialOperators, Test, Statistics
+
+# Benchmark groups
+benchmarks = BenchmarkGroup()
+
+# Put in specific benchmarks
+benchmarks["uniformReflecting"] = @benchmarkable diffusionoperators($(range(0.0, 1.0, length = 500)), $(Reflecting()), $(Reflecting()))
+benchmarks["uniformMixed"] = @benchmarkable diffusionoperators($(range(0.0, 1.0, length = 500)), $(Mixed(0.)), $(Mixed(1.)))
+benchmarks["irregularReflecting"] = @benchmarkable diffusionoperators($(collect(range(0.0, 1.0, length = 500))), $(Reflecting()), $(Reflecting()))
+benchmarks["uniformMixed"] = @benchmarkable diffusionoperators($(collect(range(0.0, 1.0, length = 500))), $(Mixed(0.)), $(Mixed(1.)))
+
+# Run and condense benchmarks
+results = run(benchmarks)
+results = median(results)
+
+# To save results, manually call in the REPL: BenchmarkTools.save("benchmarks.json", results)
+
+#Compare to old results
+try
+  oldresults= BenchmarkTools.load("benchmarks.json")[1]
+  judge(oldresults, results)
+catch err
+  error("Couldn't load file- make sure that you've previously saved results.", err.prefix)
+end
