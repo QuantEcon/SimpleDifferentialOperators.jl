987cfe3657b3f128594a455a019736a9ffc27e94
Arnav Sood <soodarnav01@gmail.com>
2019-02-28 11:45:11 -0800

    try again docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,9 @@ using Documenter, SimpleDifferentialOperators, Weave, IJulia
 
 # Generated files
 EXAMPLE = joinpath(@__DIR__, "..", "examples", "example.jmd")
-OUTPUT = joinpath(@__DIR__, "src/generated")
-Weave.notebook(EXAMPLE, OUTPUT)
+OUTPUT = joinpath(@__DIR__, "src", "generated", "example.ipynb")
+convert_doc(EXAMPLE, OUTPUT)
+Weave.notebook(OUTPUT)
 
 # Compile the online documentation.
 makedocs(sitename = "SimpleDifferentialOperators.jl",
