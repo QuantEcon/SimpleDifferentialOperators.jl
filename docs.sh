6f9e985992f75098a10b540719808499c9277b10
Arnav Sood <soodarnav01@gmail.com>
2019-03-04 13:53:32 -0800

    fix typo in script [ci skip]

--- docs.sh
+++ docs.sh
@@ -39,8 +39,6 @@ mv docs/tex/discretized-differential-operator-derivation.pdf SimpleDifferentialO
 echo "Copied PDF."
 mv docs/examples/*.ipynb SimpleDifferentialOperators.jl/$target/generated/ # move notebooks
 echo "Copied example notebooks."
-(cd SimpleDifferentialOperators.jl/$target/generated/b && touch README.md)
-echo "Set up a README."
 
 # Git operations
 (cd SimpleDifferentialOperators.jl; git add -A; git commit -m "Add generated objects to $target docs"; git push)
