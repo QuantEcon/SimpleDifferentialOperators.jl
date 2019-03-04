b9eb9275499b0d574e79c7f41b22cb8a15772ab9
Arnav Sood <soodarnav01@gmail.com>
2019-03-04 14:21:25 -0800

    update generate.jl [ci skip]

--- docs/examples/generate.jl
+++ docs/examples/generate.jl
@@ -1,6 +1,7 @@
 using Weave
 fileset = readdir(@__DIR__)
 for file in fileset
-    occursin(".jmd", file) && Weave.notebook(file, @__DIR__) && Weave.weave(file, out_path = @__DIR__, doctype = "md2html")
+    occursin(".jmd", file) && Weave.notebook(file, @__DIR__)
+    occursin(".jmd", file) && Weave.weave(file, out_path = :pwd, doctype = "md2html")
     println("Weaved $file")
 end
