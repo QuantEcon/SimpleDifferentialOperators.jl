723b605da0efa91bd022f2262d0fffe601602767
Arnav Sood <soodarnav01@gmail.com>
2019-03-04 14:05:38 -0800

    update docs/examples/generate.jl to include md2html [ci skip]

--- docs/examples/generate.jl
+++ docs/examples/generate.jl
@@ -1,6 +1,6 @@
 using Weave
 fileset = readdir(@__DIR__)
 for file in fileset
-    occursin(".jmd", file) && Weave.notebook(file, @__DIR__)
+    occursin(".jmd", file) && Weave.notebook(file, @__DIR__) && Weave.weave(file, out_path = @__DIR__, doctype = "md2html")
     println("Weaved $file")
 end
