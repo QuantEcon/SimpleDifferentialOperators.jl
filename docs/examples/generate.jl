e02ecbe4447f5d30a4e25b022ae08647bbfb39de
Arnav Sood <soodarnav01@gmail.com>
2019-03-04 15:33:18 -0800

    update generating script [ci skip]

--- docs/examples/generate.jl
+++ docs/examples/generate.jl
@@ -1,7 +1,9 @@
 using Weave
 fileset = readdir(@__DIR__)
 for file in fileset
-    occursin(".jmd", file) && Weave.notebook(file, @__DIR__)
-    occursin(".jmd", file) && Weave.weave(file, out_path = :pwd, doctype = "md2html", template = "our_template.tpl")
-    println("Weaved $file")
+    if occursin(".jmd", file)
+        Weave.notebook(file, @__DIR__)
+        Weave.weave(file, out_path = :pwd, doctype = "md2html", template = "our_template.tpl")
+        println("Weaved $file")
+    end 
 end
