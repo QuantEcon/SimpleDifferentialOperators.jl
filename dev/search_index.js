1ea21579eb71d21682d93a8e7713991f2401a252
autodocs <autodocs>
2019-03-07 19:03:47 +0000

    build based on f881976

--- dev/search_index.js
+++ dev/search_index.js
@@ -2,39 +2,39 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#",
-    "page": "﻿Installation",
-    "title": "﻿Installation",
+    "page": "﻿Usage",
+    "title": "﻿Usage",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "#Installation-1",
-    "page": "﻿Installation",
-    "title": "﻿Installation",
-    "category": "section",
-    "text": "To install, run] add SimpleDifferentialOperatorsNote that this requires Julia 1.0 or later."
-},
-
-{
     "location": "#Usage-1",
-    "page": "﻿Installation",
-    "title": "Usage",
+    "page": "﻿Usage",
+    "title": "﻿Usage",
     "category": "section",
     "text": "Consider solving for v from the following equation by the Hamilton-Jacobi-Bellman equation (HJBE):rho v(x) = f(x) + mu partial_x v(x) + fracsigma^22 partial_xx v(x)for some constant rho sigma  0 and mu leq 0. To solve v on M-size discretized grids, one can run the following code:# import LinearAlgebra package (for diagonal and identity matrices)\nusing LinearAlgebra \n# setup \nf(x) = x^2 \nμ = -0.1 # constant negative drift\nσ = 0.1\nρ = 0.05\nM = 100 # size of grid\nx = range(0.0, 1.0, length = M) # grid\n\n# discretize L = ρ - μ D_x - σ^2 / 2 D_xx\n# subject to reflecting barriers at 0 and 1\nbc = (Reflecting(), Reflecting())\nL = I * ρ - μ*L₁₋(x, bc) - σ^2 / 2 * L₂(x, bc)\n## solve the value function\nv_bc = L \\ f.(x) Note that the code above uses differential operators with reflecting boundary conditions applied.  One can alternatively use differential operators on interior nodes and stack them with matrices for boundary conditions to compute v:# operators without boundary conditions, adding extra two rows for boundary conditions\n## differential operators on extended nodes\nA = μ*L̄₁₋(x) + σ^2 / 2 * L̄₂(x)\n## matrix for boundary conditions\nB = transpose([[-1; 1; zeros(M)] [zeros(M); -1; 1]]) \n## stack them together\nL = [([zeros(M) Diagonal(ones(M,M)) zeros(M)] * 0.05 - A); B] \n## solve the value function with reflecting barrier bc (last two elements)\nv_bar = L \\ [f.(x); 0.0; 0.0] \n## extract the interior (is identical with `v_bc` above)\nv_interior = v_bar[2:end-1] "
 },
 
 {
-    "location": "#Examples-1",
-    "page": "﻿Installation",
-    "title": "Examples",
+    "location": "#Installation-1",
+    "page": "﻿Usage",
+    "title": "Installation",
+    "category": "section",
+    "text": "To install, run] add SimpleDifferentialOperatorsNote that this requires Julia 1.0 or later."
+},
+
+{
+    "location": "#Usage-Examples-1",
+    "page": "﻿Usage",
+    "title": "Usage Examples",
     "category": "section",
     "text": ""
 },
 
 {
     "location": "#Solving-HJBE-with-state-dependent-drift-variables-1",
-    "page": "﻿Installation",
+    "page": "﻿Usage",
     "title": "Solving HJBE with state-dependent drift variables",
     "category": "section",
     "text": "One can also deploy upwind schemes when drift variable is not constant. Consider solving for v from the following Bellman equation:rho v(x) = f(x) + mu(x) partial_x v(x) + fracsigma^22 partial_xx v(x)associated with the diffusion processdx = mu(x) dt + sigma dWfor some constant rho sigma  0 and mu(x) = -x. Note that mu(x) depends on states. The following code will solve v using upwind schemes:# setup \nf(x) = x^2 \nμ(x) = -x # drift depends on state\nσ = 1.0\nρ = 0.05\nM = 100 # size of grid\nx = range(-1.0, 1.0, length = 100)\n\nbc = (Reflecting(), Reflecting())\n\n# Define first order differential operator using upwind scheme\nL₁ = Diagonal(min.(μ.(x), 0.0)) * L₁₋(x, bc) + Diagonal(max.(μ.(x), 0.0)) * L₁₊(x, bc)\n\n# Define linear operator using upwind schemes\nL = L₁ - σ^2 / 2 * L₂(x,bc)\n\n# solve the value function\nv_bc = (I * ρ - L) \\ f.(x) "
@@ -42,10 +42,10 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#Finding-stationary-distribution-from-the-Kolmogorov-forward-equation-(KFE)-1",
-    "page": "﻿Installation",
+    "page": "﻿Usage",
     "title": "Finding stationary distribution from the Kolmogorov forward equation (KFE)",
     "category": "section",
-    "text": "One can also compute the stationary distribution of the state x above from the corresponding KFE:partial_t g(xt) = - mu(x) partial_x g(x t) + fracsigma^22 partial_xx g(xt)by taking partial_t g(xt) = 0, i.e., solving g from the L^* g(x) = 0 whereL^* = - mu(x) partial_x + fracsigma^22 partial_xxBy descretizing the space of x, one can solve the corresponding system by using discretized operators for L^*. Note that the operator for the KFE in the original equation is the adjoint operator of the operator for the HJBE, L, and the correct discretization scheme for L^* is, analogously, done by taking the transpose of the discretized operator for HJBE, L (See Gabaix et al., 2016). Hence, one can find the stationary distribution by solving the following discretized system of equations:L^T g = 0such that the sum of g is one. This can be found by finding a non-trivial eigenvector for L^T  associated with the eigenvalue of zero:using Arpack # library for extracting eigenvalues and eigenvectors\n\n# extract eigenvalues and eigenvectors, smallest eigenval in magintute first\nλ, ϕ = eigs(transpose(L), which = :SM); \n# extract the very first eigenvector (associated with the smallest eigenvalue)\ng_ss = real.(ϕ[:,1]);\n# normalize it\ng_ss = g_ss / sum(g_ss)"
+    "text": "One can also compute the stationary distribution of the state x above from the corresponding KFE:partial_t g(xt) = - mu(x) partial_x g(x t) + fracsigma^22 partial_xx g(xt)by taking partial_t g(xt) = 0, i.e., solving g from the L^* g(x) = 0 whereL^* = - mu(x) partial_x + fracsigma^22 partial_xxBy descretizing the space of x, one can solve the corresponding system by using discretized operators for L^*. Note that the operator for the KFE in the original equation is the adjoint operator of the operator for the HJBE, L, and the correct discretization scheme for L^* is, analogously, done by taking the transpose of the discretized operator for HJBE, L (See Gabaix et al., 2016 and Achdou et al., 2017). Hence, one can find the stationary distribution by solving the following discretized system of equations:L^T g = 0such that the sum of g is one. This can be found by finding a non-trivial eigenvector for L^T  associated with the eigenvalue of zero:using Arpack # library for extracting eigenvalues and eigenvectors\n\n# extract eigenvalues and eigenvectors, smallest eigenval in magintute first\nλ, ϕ = eigs(transpose(L), which = :SM); \n# extract the very first eigenvector (associated with the smallest eigenvalue)\ng_ss = real.(ϕ[:,1]);\n# normalize it\ng_ss = g_ss / sum(g_ss)"
 },
 
 {
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Mixed Boundary Values",
     "category": "section",
-    "text": "Under M-length of grids on v with end points x_min  x_max with mixed boundary value conditions ofmath \\underline{\\xi} v(x{\\min}) + \\nabla v(x{\\min}) &= 0\\\n\\overline{\\xi} v(x{\\max}) + \\nabla v(x{\\max}) &= 0Note that this can be extended to reflecting boundary conditions by assigning underlinexi = 0 and overlinexi = 0. We use the following discretization schemes:L_1- equiv frac1Deltabeginpmatrix\n1 - (1 + underlinexi Delta) 00dots000\n-110dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots-110\n000cdots0-11\nendpmatrix_Mtimes ML_1+ equiv frac1Deltabeginpmatrix\n-110dots000\n0-11dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots0-11\n000cdots00-1+(1-overlinexi Delta)\nendpmatrix_Mtimes MlabeleqL-1-plus-regular L_2 equiv frac1Delta^2beginpmatrix\n-2 + (1 + underlinexi Delta) 10dots000\n1-21dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots1-21\n000cdots01-2 + (1- overlinexi Delta)\nendpmatrix_Mtimes MlabeleqL-2-regularwhich represent the backward first order, foward first order, and central second order differential operators respectively."
+    "text": "Under M-length of grids on v with end points x_min  x_max with mixed boundary value conditions ofbeginalign\nunderlinexi v(x_min) + nabla v(x_min) = 0 \noverlinexi v(x_max) + nabla v(x_max) = 0\nendalignNote that this can be extended to reflecting boundary conditions by assigning underlinexi = 0 and overlinexi = 0. We use the following discretization schemes:L_1- equiv frac1Deltabeginpmatrix\n1 - (1 + underlinexi Delta) 00dots000\n-110dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots-110\n000cdots0-11\nendpmatrix_Mtimes ML_1+ equiv frac1Deltabeginpmatrix\n-110dots000\n0-11dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots0-11\n000cdots00-1+(1-overlinexi Delta)\nendpmatrix_Mtimes MlabeleqL-1-plus-regular L_2 equiv frac1Delta^2beginpmatrix\n-2 + (1 + underlinexi Delta) 10dots000\n1-21dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots1-21\n000cdots01-2 + (1- overlinexi Delta)\nendpmatrix_Mtimes MlabeleqL-2-regularwhich represent the backward first order, foward first order, and central second order differential operators respectively."
 },
 
 {
@@ -101,7 +101,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Applying boundary conditions with operators on extended grids",
     "category": "section",
-    "text": "Boundary conditions can be applied manually by using operators on extended grids. This can be done by first extending x = x_i_i=1^M to $ \\overline{x} = {xi}{i=0}^{M+1}$ where x_i+1 - x_i = Delta for some constant Delta  0 for all i = 0  M. We call x_0 and x_M+1, extra nodes just before and after x_min and x_max, as ghost nodes. Likewise, define v(overlinex) as (M+2)-vector whose ith element is overline x_i. We can then define the following operators on overlinex:overlineL_1- equiv frac1Deltabeginpmatrix\n-110dots000\n0-11dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots100\n000cdots-110\nendpmatrix_Mtimes (M+2)overlineL_1+ equiv frac1Deltabeginpmatrix\n0-11dots000\n00-1dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots-110\n000cdots0-11\nendpmatrix_Mtimes (M+2) overlineL_2 equiv frac1Delta^2beginpmatrix\n-12-1dots000\n0-12dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots2-10\n000cdots-121\nendpmatrix_Mtimes (M+2)Suppose that we want to solve a system L v(x) = f(x)  where L is a linear combination of discretized differential operators for some f(x) that represents the values of a function f on discretized x. To solve the system under boundary conditions on v, one can construct and solve the following extended system:beginbmatrix\noverlineL \nB\nendbmatrix \nv(overlinex) = \nbeginbmatrix\nf(x) \nb\nendbmatrix with M_E by (M+2) matrix B and M_E-length vector b that represent the current boundary conditions, where M_E is the number of boundary conditions to be applied. For instance, to apply reflecting barrier conditions v(x_min) = v(x_max) = 0, one can useB = beginbmatrix\n1  -1  0  dots  0  0  0 \n0  0  0  dots  0  -1  1\nendbmatrix_2 times (M+2) quad \nb = beginbmatrix\n0 \n0\nendbmatrixLikewise, for mixed boundary conditions v(x_min) = underlinexi and v(x_max) = overlinexi, one can useB = beginbmatrix\n1  -1  0  dots  0  0  0 \n0  0  0  dots  0  -1  1\nendbmatrix_2 times (M+2) quad \nb = beginbmatrix\n underlinexi Delta \noverlinexi Delta\nendbmatrix"
+    "text": "Boundary conditions can be applied manually by using operators on extended grids. This can be done by first extending x = x_i_i=1^M to $ \\overline{x} = {xi}{i=0}^{M+1}$ where x_i+1 - x_i = Delta for some constant Delta  0 for all i = 0  M. We call x_0 and x_M+1, extra nodes just before and after x_min and x_max, as ghost nodes. Likewise, define v(overlinex) as (M+2)-vector whose ith element is overline x_i. We can then define the following operators on overlinex:overlineL_1- equiv frac1Deltabeginpmatrix\n-110dots000\n0-11dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots100\n000cdots-110\nendpmatrix_Mtimes (M+2)overlineL_1+ equiv frac1Deltabeginpmatrix\n0-11dots000\n00-1dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots-110\n000cdots0-11\nendpmatrix_Mtimes (M+2) overlineL_2 equiv frac1Delta^2beginpmatrix\n-12-1dots000\n0-12dots000\nvdotsvdotsvdotsddotsvdotsvdotsvdots\n000dots2-10\n000cdots-121\nendpmatrix_Mtimes (M+2)Suppose that we want to solve a system L v(x) = f(x)  where L is a linear combination of discretized differential operators for some f(x) that represents the values of a function f on discretized x. To solve the system under boundary conditions on v, one can construct and solve the following extended system:beginbmatrix\noverlineL \nB\nendbmatrix \nv(overlinex) = \nbeginbmatrix\nf(x) \nb\nendbmatrix with M_E by (M+2) matrix B and M_E-length vector b that represent the current boundary conditions, where M_E is the number of boundary conditions to be applied. For instance, to apply reflecting barrier conditions v(x_min) = v(x_max) = 0, one can useB = beginbmatrix\n1  -1  0  dots  0  0  0 \n0  0  0  dots  0  -1  1\nendbmatrix_2 times (M+2) quad \nb = beginbmatrix\n0 \n0\nendbmatrixLikewise, for non-homogenous boundary conditions v(x_min) = underlinexi and v(x_max) = overlinexi, one can useB = beginbmatrix\n1  -1  0  dots  0  0  0 \n0  0  0  dots  0  -1  1\nendbmatrix_2 times (M+2) quad \nb = beginbmatrix\n underlinexi Delta \noverlinexi Delta\nendbmatrix"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Hamilton–Jacobi–Bellman equations (HJBE)",
     "category": "section",
-    "text": "Consider solving for v from the following optimal control problemv(x_0) = max_ alpha(t)  _t geq 0  int_0^infty e^-rho t r( x(t) alpha(t )) dtwith the law of motion for the state dx = mu dt + sigma dW for some constant mu geq 0 and sigma geq 0 with x(0) = x_0.Let alpha^*(t) be the optimal solution. Suppose that r under alpha^*(t) can be expressed in terms of state variables, r^* (x). Then, the HJBE yields\\rho v(x) = r^*(x) +  \\mu  \\partial_{x} v(x) + \\dfrac{\\sigma^2}{2} \\partial_{xx} v(x)In terms of differential operators, one can rewrite the equation as(\\rho - \\tilde{L}) v(x) = r^*(x)where \\tilde{L} = \\mu \\partial_{x} + (\\sigma^2/2) \\partial_{xx}By descretizing the space of x, one can solve the corresponding system by using discretized operators for partial_x (L_1+), partial_xx (L_2) on some grids of length M, x_i_i=1^M:L = mu L_1+ + dfracsigma^22 L_2so that v under the optimal plan can be computed by solving the following discretized system of equations:(rho I - L) v = r^*where v and r^* are M-vectors whose ith elements are v(x_i) and r^*(x_i), respectively."
+    "text": "Consider solving for v from the following optimal control problemv(x_0) = max_ alpha(t)  _t geq 0  int_0^infty e^-rho t r( x(t) alpha(t )) dtwith the law of motion for the state dx = mu dt + sigma dW for some constant mu geq 0 and sigma geq 0 with x(0) = x_0.Let alpha^*(t) be the optimal solution. Suppose that r under alpha^*(t) can be expressed in terms of state variables, r^* (x). Then, the HJBE yieldsrho v(x) = r^*(x) +  mu  partial_x v(x) + dfracsigma^22 partial_xx v(x)In terms of differential operators, one can rewrite the equation as(rho - tildeL) v(x) = r^*(x)where tildeL = mu partial_x + (sigma^22) partial_xxBy descretizing the space of x, one can solve the corresponding system by using discretized operators for partial_x (L_1+), partial_xx (L_2) on some grids of length M, x_i_i=1^M:L = mu L_1+ + dfracsigma^22 L_2so that v under the optimal plan can be computed by solving the following discretized system of equations:(rho I - L) v = r^*where v and r^* are M-vectors whose ith elements are v(x_i) and r^*(x_i), respectively."
 },
 
 {
@@ -125,7 +125,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Kolmogorov forward equations (KFE) under diffusion process",
     "category": "section",
-    "text": "Let g(x t) be the distribution of x at time t from the example above. By the Kolmogorov forward equation, the following PDE holds:\\partial_{t} g(x, t) = - \\mu \\partial_{x}  g(x,t) + \\dfrac{\\sigma^2}{2} \\partial_{xx} g(x,t)"
+    "text": "Let g(x t) be the distribution of x at time t from the example above. By the Kolmogorov forward equation, the following PDE holds:partial_t g(x t) = - mu partial_x  g(xt) + dfracsigma^22 partial_xx g(xt)"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Stationary distributions",
     "category": "section",
-    "text": "The stationary distribution g^*(x) satisfies0 = - mu partial_x g^*(x) + dfracsigma^22 partial_xx g^*(x)which can be rewritten as tildeL^* g(x) = 0where tildeL^* =  - mu partial_x + (sigma^22) partial_xxBy descretizing the space of x, one can solve the corresponding system by using discretized operators for tildeL^*. Note that the operator for the KFE in the original equation is the adjoint operator of the operator for the HJBE, tildeL, and the correct discretization scheme for L^* is, analogously, done by taking the transpose of the discretized operator for HJBE, L (See Gabaix et al., 2016). Hence, one can find the stationary distribution by solving the following discretized system of equations:L^T g = 0 where L^T is the transpose of L and g is an M-vector whose element is g(x_i) such that sum_i=1^M g(x_i) = 1."
+    "text": "The stationary distribution g^*(x) satisfies0 = - mu partial_x g^*(x) + dfracsigma^22 partial_xx g^*(x)which can be rewritten as tildeL^* g(x) = 0where tildeL^* =  - mu partial_x + (sigma^22) partial_xxBy descretizing the space of x, one can solve the corresponding system by using discretized operators for tildeL^*. Note that the operator for the KFE in the original equation is the adjoint operator of the operator for the HJBE, tildeL, and the correct discretization scheme for tildeL^* is, analogously, done by taking the transpose of the discretized operator for HJBE, L (See Gabaix et al., 2016 and Achdou et al., 2017). Hence, one can find the stationary distribution by solving the following discretized system of equations:L^T g = 0 where L^T is the transpose of L and g is an M-vector whose element is g(x_i) such that sum_i=1^M g(x_i) = 1."
 },
 
 {
@@ -141,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Derivations",
     "title": "Full dynamics of distributions",
     "category": "section",
-    "text": "One can also solve the full PDE in KFE equation, given an initial distribution g(x 0). After discretization, note that \\eqref{eq:kfe} can be rewritten asdotg(t) = L^T g(t)where dotg(t) is an M-vector whose ith element is partial_t g(x_i t), which can be efficently solved by a number of differential equation solvers available in public, including DifferentialEquations.jl."
+    "text": "One can also solve the full PDE in KFE equation, given an initial distribution g(x 0). After discretization, note that the KFE can be rewritten asdotg(t) = L^T g(t)where dotg(t) is an M-vector whose ith element is partial_t g(x_i t), which can be efficently solved by a number of differential equation solvers available in public, including DifferentialEquations.jl."
 },
 
 {
