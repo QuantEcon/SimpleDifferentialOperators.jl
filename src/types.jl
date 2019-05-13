baae1d8a97cac072c9ebd56bac6c7ea104b271e3
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2019-05-13 15:18:23 -0700

    Add a constructor for `JumpProcess` with `jumpf` that defines the size of a jump

--- src/types.jl
+++ src/types.jl
@@ -31,6 +31,30 @@ function JumpProcess(x̄::AbstractArray, jumps::AbstractArray,
 
     return JumpProcess(jumps_after_truncation)
 end
+function JumpProcess(x̄::AbstractArray, jumpf::Function, 
+    truncate = (:interior, :interior))
+    # each ith element of jumps defines the jump direction (negative/positive) and size
+    # where i ranges from 1 to M (interior points)
+    x = interiornodes(x̄)
+    M = length(x)
+
+    # define default destinations and truncation indices
+    destinations = x + jumpf.(x)
+
+    cutoff_lb = (truncate[1] == :interior) ? x[1] : x̄[1] 
+    cutoff_ub = (truncate[2] == :interior) ? x[end] : x̄[end]
+
+    # perform truncations
+    destinations[destinations .< cutoff_lb] .= cutoff_lb
+    destinations[destinations .> cutoff_ub] .= cutoff_ub
+
+    # find the corresponding destination indices and jump indices 
+    destinations_indices_on_x̄ = (destination -> findnearestindex(x̄, destination)).(destinations)
+    current_indices_on_x̄ = Array(2:(M+1))
+    jumps_after_truncation = destinations_indices_on_x̄ - current_indices_on_x̄
+    
+    return JumpProcess(jumps_after_truncation)
+end
 
 # Concretes
 struct Reflecting <: HomogeneousBoundaryCondition end
--- test/jump-diffusion.jl
+++ test/jump-diffusion.jl
@@ -2,7 +2,7 @@ using SimpleDifferentialOperators
 using Test, LinearAlgebra, DualNumbers
 
 
-@testset "Truncation rules" begin
+@testset "Truncation rules when indices are explicitly given" begin
     x̄ = 1:5
     jumps = [0; -1; 0]
     @test JumpProcess(x̄, jumps).jumps == jumps 
@@ -51,4 +51,72 @@ using Test, LinearAlgebra, DualNumbers
     @test JumpProcess(x̄, jumps, (:interior, :boundary)).jumps == [0; 2; 1]
     @test JumpProcess(x̄, jumps, (:boundary, :interior)).jumps == [-1; 1; 0] 
     @test JumpProcess(x̄, jumps, (:boundary, :boundary)).jumps == jumps
+end
+
+@testset "Truncation rules when jump functions are provided" begin
+    x̄ = 1:5
+    jumps = [0; -1; 0]
+    jumpf = x -> (x == 3) ? -1 : 0 
+    @test JumpProcess(x̄, jumpf).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps 
+
+    jumps = [0; 1; 0]
+    jumpf = x -> (x == 3) ? 1 : 0 
+    @test JumpProcess(x̄, jumpf).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps 
+
+    jumps = [0; -2; 0]
+    jumpf = x -> (x == 3) ? -2 : 0 
+    @test JumpProcess(x̄, jumpf).jumps == [0; -1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == [0; -1; 0]
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps 
+    
+    jumps = [0; 2; 0]
+    jumpf = x -> (x == 3) ? 2 : 0 
+    @test JumpProcess(x̄, jumpf).jumps == [0; 1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == jumps
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == [0; 1; 0] 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps
+    
+    jumps = [-1; -2; 0]
+    jumpf = x -> (x <= 3) ? -(x-1) : 0 
+    @test JumpProcess(x̄, jumpf).jumps == [0; -1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == [0; -1; 0]
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == jumps 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps 
+    
+    jumps = [0; 2; 1]
+    jumpf = x -> (x >= 3) ? (5-x) : 0 
+    @test JumpProcess(x̄, jumpf).jumps == [0; 1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == jumps
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == [0; 1; 0] 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps
+    
+    jumps = [-1; -2; 1]
+    jumpf = x -> (x <= 3) ? -(x-1) : 1
+    @test JumpProcess(x̄, jumpf).jumps == [0; -1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == [0; -1; 1]
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == [-1; -2; 0]
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps
+    
+    jumps = [-1; 2; 1]
+    jumpf = x -> (x >= 3) ? (5-x) : -1
+    @test JumpProcess(x̄, jumpf).jumps == [0; 1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == [0; 2; 1]
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == [-1; 1; 0] 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps
+
+    # on irregular grids
+    x̄ = [1; 2.5; 3; 4.9; 5]
+    jumps = [-1; 2; 1]
+    jumpf = x -> (x >= 3) ? (5-x) : -1
+    @test JumpProcess(x̄, jumpf).jumps == [0; 1; 0]
+    @test JumpProcess(x̄, jumpf, (:interior, :boundary)).jumps == [0; 2; 1]
+    @test JumpProcess(x̄, jumpf, (:boundary, :interior)).jumps == [-1; 1; 0] 
+    @test JumpProcess(x̄, jumpf, (:boundary, :boundary)).jumps == jumps
 end
