c3304b7a39e0e39f93cb6f15e2546e5301c50da6
Chiyoung Ahn <chiyahn@alumni.ubc.ca>
2019-05-21 10:35:46 -0700

    Add docs to `jointoperator_bc`

--- src/jointoperators.jl
+++ src/jointoperators.jl
@@ -1,4 +1,49 @@
-function jointoperator_bc(operators, Q)
+"""
+    jointoperator_bc(operators, Q::Array)
+
+Returns a discretized operator that solves systems of differential equations defined by
+`operators` with transitions by `Q` where `operators` is an N-length collection of 
+discretized operators with boundary conditions applied and `Q` is N by N matrix of transition
+probabilities whose (i,j)th element defines the probability of visiting jth state from ith state
+
+# Examples
+```jldoctest; setup = :(using SimpleDifferentialOperators)
+julia> x̄ = 0:5
+0:5
+
+julia> L1bc = L₁₋bc(x̄, (Reflecting(), Reflecting()))
+4×4 LinearAlgebra.Tridiagonal{Float64,Array{Float64,1}}:
+  0.0   0.0    ⋅    ⋅
+ -1.0   1.0   0.0   ⋅
+   ⋅   -1.0   1.0  0.0
+   ⋅     ⋅   -1.0  1.0
+
+julia> L2bc = L₂bc(x̄, (Reflecting(), Reflecting()))
+4×4 LinearAlgebra.Tridiagonal{Float64,Array{Float64,1}}:
+ -1.0   1.0    ⋅     ⋅
+  1.0  -2.0   1.0    ⋅
+   ⋅    1.0  -2.0   1.0
+   ⋅     ⋅    1.0  -1.0
+
+julia> Q = [0.5 0.5; 0.3 0.7]
+2×2 Array{Float64,2}:
+ 0.5  0.5
+ 0.3  0.7
+
+julia> jointoperator_bc((L1bc, L2bc), Q)
+2×2-blocked 8×8 BlockBandedMatrices.BandedBlockBandedMatrix{Float64,BlockArrays.PseudoBlockArray{Float64,2,Array{Float64,2},BlockArrays.BlockSizes{2,Tuple{Array{Int64,1},Array{Int64,1}}}}}:
+  0.5   0.0    ⋅    ⋅   │   0.5   0.0    ⋅     ⋅
+ -1.0   1.5   0.0   ⋅   │   0.0   0.5   0.0    ⋅
+   ⋅   -1.0   1.5  0.0  │    ⋅    0.0   0.5   0.0
+   ⋅     ⋅   -1.0  1.5  │    ⋅     ⋅    0.0   0.5
+ ───────────────────────┼────────────────────────
+  0.3   0.0    ⋅    ⋅   │  -0.3   1.0    ⋅     ⋅
+  0.0   0.3   0.0   ⋅   │   1.0  -1.3   1.0    ⋅
+   ⋅    0.0   0.3  0.0  │    ⋅    1.0  -1.3   1.0
+   ⋅     ⋅    0.0  0.3  │    ⋅     ⋅    1.0  -0.3
+```
+"""
+function jointoperator_bc(operators, Q::Array)
     M = size(operators[1],1)
     N = length(operators)
 
