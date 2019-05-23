fe81d29a1011c2bcb39f4edf40a00955c59bb775
Arnav Sood <soodarnav01@gmail.com>
2019-05-23 16:04:06 -0700

    re-enable KFE

--- docs/examples/generate.jl
+++ docs/examples/generate.jl
@@ -1,7 +1,7 @@
 using Weave
 fileset = readdir(@__DIR__)
 for file in fileset
-    if occursin(".jmd", file) && ~occursin("KFE", file)
+    if occursin(".jmd", file) 
         Weave.notebook(file)
         Weave.weave(file, out_path = :pwd, doctype = "md2html", template = "our_template.tpl")
         println("Weaved $file")
