cbc5d5208f408256a1d55925440d04c83449f25f
PooyaFa <pooyaravari@gmail.com>
2019-09-13 19:20:26 -0700

    Just Tried to write the code in a test format
    I am not sure if it's a better way or not, I thought maybe it's more clear and I looked at tests in DiffEqOperators tried to make this one looks similar.

--- test/DiffEqOperators/BasicSDOExamples.jl
+++ test/DiffEqOperators/BasicSDOExamples.jl
@@ -1,55 +1,51 @@
 
-using LinearAlgebra, DiffEqOperators, SimpleDifferentialOperators
+using LinearAlgebra, SimpleDifferentialOperators, DiffEqOperators, Test, Parameters
+@testset "Constant Drifts" begin
+    #parameters
+    params = @with_kw (
+        μ = -0.1, # constant negative drift
+        σ = 0.1,
+        ρ = 0.05,
+        M = 100, # size of grid (interior points)
+        x̄ = range(0.0, 1.0, length = (M+2)),
+        x = interiornodes(x̄) # i.e., x̄[2:end-1]
+    )
+    p = params();
+    # payoff function
+    π(x) = x^2
+    # Below is a basic example from SDO:
+    function SDO(π, params)
+        x = interiornodes(params.x̄) # i.e., x̄[2:end-1]
 
+        # discretize L = ρ - μ D_x - σ^2 / 2 D_xx
+        # subject to reflecting barriers at 0 and 1
+        bc = (Reflecting(), Reflecting())
+        Lₓ = params.μ*L₁₋bc(params.x̄, bc) + params.σ^2 / 2 * L₂bc(params.x̄, bc)
+        L_bc = I * params.ρ - Lₓ
 
-# Below is a basic example from SDO:
+        # solve the value function
+        v = L_bc \ π.(x);
+        return v
+    end
 
-# setup
-π(x) = x^2
-μ = -0.1 # constant negative drift
-σ = 0.1
-ρ = 0.05
-M = 100 # size of grid (interior points)
+    # The DiffEq approach (as of the current implementation)
+    function DEO(π, params)
+        x = interiornodes(params.x̄)
+        dx = x[2] - x[1]
+        # discretize L = ρ - μ D_x - σ^2 / 2 D_xx
+        # subject to reflecting barriers at 0 and 1
+        L1 = UpwindDifference(1,1,dx,params.M,t->1.0)
+        L2 = CenteredDifference(2,2,dx,params.M)
+        Q = Neumann0BC(dx, 1)
+        # Here Array(A::GhostDerivativeOperator) will return a tuple of the linear part
+        # and the affine part of the operator A, hence we index Array(µ*L1*Q).
+        # The operators in this example are purely linear, so we don't worry about Array(µ*L1*Q)[2]
+        Lₓ = Array(params.µ*L1*Q)[1] + Array(params.σ^2/2*L2*Q)[1]
+        L_bc = I * params.ρ - Lₓ
 
-x̄ = range(0.0, 1.0, length = (M+2))
-x = interiornodes(x̄) # i.e., x̄[2:end-1]
-
-# discretize L = ρ - μ D_x - σ^2 / 2 D_xx
-# subject to reflecting barriers at 0 and 1
-bc = (Reflecting(), Reflecting())
-Lₓ = μ*L₁₋bc(x̄, bc) + σ^2 / 2 * L₂bc(x̄, bc)
-L_bc = I * ρ - Lₓ
-
-# solve the value function
-v = L_bc \ π.(x)
-
-
-# The DiffEq approach (as of the current implementation)
-
-# setup
-π(x) = x^2
-μ = -0.1 # constant negative drift
-σ = 0.1
-ρ = 0.05
-M = 100 # size of grid (interior points)
-
-x̄ = range(0.0, 1.0, length = (M+2))
-x = interiornodes(x̄) # i.e., x̄[2:end-1]
-dx = x[2] - x[1]
-
-# discretize L = ρ - μ D_x - σ^2 / 2 D_xx
-# subject to reflecting barriers at 0 and 1
-
-L1 = UpwindDifference(1,1,dx,M,t->1.0)
-L2 = CenteredDifference(2,2,dx,M)
-Q = Neumann0BC(dx, 1)
-
-# Here Array(A::GhostDerivativeOperator) will return a tuple of the linear part
-# and the affine part of the operator A, hence we index Array(µ*L1*Q).
-# The operators in this example are purely linear, so we don't worry about Array(µ*L1*Q)[2]
-
-Lₓ = Array(µ*L1*Q)[1] + Array(σ^2/2*L2*Q)[1]
-L_bc = I * ρ - Lₓ
-
-# solve the value function
-v = L_bc \ π.(x)
+        # solve the value function
+        v = L_bc \ π.(x);
+        return v
+    end
+    @test DEO(π, p) ≈ SDO(π, p)
+end
